639e5a3935d53fcb71e8fbc967fe7ae2a8790a25
CORP\pkuma834 <151085priti@gmail.com>
2022-03-29 12:55:33 +0800

    first commit

---
+++ src/main/java/com/appliance/appliance/service/ApplianceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appliance.appliance.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplianceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplianceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appliance/appliance/service/ApplianceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appliance.appliance.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplianceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
