9944450b0e74577963d2a22dab4a957f4ccfccb2
CORP\pkuma834 <151085priti@gmail.com>
2022-03-29 13:19:52 +0800

    package name changed and jpa comment

--- src/main/java/com/appliance/ApplianceServiceApplication.java
+++ src/main/java/com/appliance/ApplianceServiceApplication.java
@@ -1,4 +1,4 @@
-package com.appliance.appliance.service;
+package com.appliance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
