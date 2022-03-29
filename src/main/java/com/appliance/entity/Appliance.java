58b0535de5e3386b05a08e2aaf036064675c5c11
CORP\pkuma834 <151085priti@gmail.com>
2022-03-29 16:14:37 +0800

    h2 database properties added

--- src/main/java/com/appliance/entity/Appliance.java
+++ src/main/java/com/appliance/entity/Appliance.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Setter
 @Entity
 @NoArgsConstructor
+@Table(name="HOUSEHOLD")
 @AllArgsConstructor
 public class Appliance {
 	@Id
